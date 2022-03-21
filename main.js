3d3c6e688a62d3bbdd1f5d61e57549bbc5a834e8
Aanya3528 <75934019+Aanya3528@users.noreply.github.com>
2022-03-21 13:24:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+
+    img=loadImage('Sorting hat.jpg');
+
+}
+
+function setup(){
+
+canvas= createCanvas(300,300);
+canvas.center();
+
+}
+
+function draw(){
+
+
+
+}
+
+function take_snapshot(){
+
+save("Filter Image.jpg")
+
+}
